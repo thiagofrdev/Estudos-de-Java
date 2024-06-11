67ec001230f5af0fac8ae6147c7bcac8a9a73aec
thiagofrdev <thiago26ribeiro@gmail.com>
2024-06-11 15:43:06 -0300

    Estudando Enumerações

---
+++ Cursos/CursoUdemy_NelioAlves/Seção 13 - Enumerações, composição/enumeracoes/src/entities/Order.java
@@ -0,0 +1,49 @@
+package entities;
+
+import java.util.Date;
+import entities.enums.OrderStatus;
+
+public class Order {
+	private Integer id;
+	private Date moment;
+	private OrderStatus status;
+	
+	public Order(Integer id, Date moment, OrderStatus status) {
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	
+	public String toString() {
+		return "ID = "
+				+ id
+				+"\nDATE = "
+				+ moment
+				+"\nSTATUS = "
+				+ status;
+	}
+}
---
+++ Cursos/CursoUdemy_NelioAlves/Seção 13 - Enumerações, composição/enumeracoes/src/entities/enums/OrderStatus.java
@@ -0,0 +1,15 @@
+package entities.enums;
+
+/*
+ * "Enum é uma palavra curta para "enumeration" (enumeração). 
+ * Pense nisso como uma forma de organizar e dar nomes a um grupo de 
+ * coisas que são parecidas ou que pertencem a uma mesma categoria." * 
+ * ChatGPT
+ */
+
+public enum OrderStatus {
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+}
