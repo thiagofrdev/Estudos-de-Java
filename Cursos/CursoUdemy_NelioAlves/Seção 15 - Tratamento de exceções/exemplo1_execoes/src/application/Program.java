0ae01c18e71a419f0c65d9e53065e69d06f7f26b
thiagofrdev <thiago26ribeiro@gmail.com>
2024-06-18 15:03:27 -0300

    Modo ruim de fazer tratamento de exceções

--- Cursos/CursoUdemy_NelioAlves/Seção 15 - Tratamento de exceções/exemplo1_execoes/src/application/Program.java
+++ Cursos/CursoUdemy_NelioAlves/Seção 15 - Tratamento de exceções/exemplo1_execoes/src/application/Program.java
@@ -33,14 +33,13 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(scan.next());
 			
-			Date now = new Date();
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			} else if(!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			/***A melhora obtida na lógica foi delegar o tratamento do erro para sua respectiva classe***/
+			
+			String error = reservation.updateDates(checkIn, checkOut);
+			if(error != null) {
+				System.out.println("Error in reservation: " + error);
 			} else {
-				reservation.updateDates(checkIn, checkOut);
-				System.out.println("Reservation: " + reservation);
+				System.out.println("Reservation: " + error);
 			}
 		}
 		scan.close();
