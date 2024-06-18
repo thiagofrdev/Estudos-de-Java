317c0f0f4dbf682d2042e4f6424402e5b0b9700e
thiagofrdev <thiago26ribeiro@gmail.com>
2024-06-18 14:09:45 -0300

    Estudando tratamento de exceções

---
+++ Cursos/CursoUdemy_NelioAlves/Seção 15 - Tratamento de exceções/exemplo1_execoes/src/entities/Reservation.java
@@ -0,0 +1,54 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	
+	//Está como static para não ficar criando novos "sdf" para cada objeto criado. O "sdf" só vai ser criado uma vez 
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	//Pega a diferença, em milisegundos, entre as datas e deposi transforma para dias
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room "
+			+ roomNumber
+			+ ", check-in: "
+			+ sdf.format(checkIn)
+			+ ", check-out: "
+			+ sdf.format(checkOut)
+			+ ", "
+			+ duration()
+			+ " nights";
+	}
+}
+
+
