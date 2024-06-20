c1dd8d23daf6294ca94600716b72e58f78ce2ac6
thiagofrdev <thiago26ribeiro@gmail.com>
2024-06-20 14:16:36 -0300

    Placing pieces on the board

--- Cursos/CursoUdemy_NelioAlves/Seção 16 - PROJETO - Sistema jogo de xadrez/chess-system/src/boardgame/Board.java
+++ Cursos/CursoUdemy_NelioAlves/Seção 16 - PROJETO - Sistema jogo de xadrez/chess-system/src/boardgame/Board.java
@@ -8,7 +8,7 @@ public class Board {
 	public Board(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
-		pieces = new Piece[rows][columns];
+		pieces = new Piece[rows][columns];//Instanciação da matriz de peças
 	}
 
 	public int getRows() {
@@ -34,5 +34,10 @@ public class Board {
 	//Sobrecarga do método acima
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getCollumn()];
+	}
+	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getCollumn()] = piece;//Pega a matriz, na posição passada, e atribui a peça informada no método
+		piece.position = position;//Essa chamda só ocorre por que na classe Piece o "position" foi declarado como protected. Podendo ser acessado apenas classes do pacote em que está
 	}
 }
